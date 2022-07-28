732a01637ef061858ccd3fc39f5015e74a8bc70f
Godwin <godwinjoseph.k@gmail.com>
2022-07-28 15:30:31 +0530

    test comment

--- app/src/main/java/com/google/samples/apps/sunflower/data/AppDatabase.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/data/AppDatabase.kt
@@ -32,6 +32,7 @@ import com.google.samples.apps.sunflower.workers.SeedDatabaseWorker.Companion.KE
 
 /**
  * The Room database for this app
+ * godwin
  */
 @Database(entities = [GardenPlanting::class, Plant::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
